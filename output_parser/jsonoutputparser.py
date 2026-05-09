51aafb7d89d51339ec2fe6189e14879f22666ab0
Kabir <pmskabir123@gmail.com>
2026-05-09 22:46:15 +0530

    used free llm by hf to test jsonoutputparser

--- output_parser/jsonoutputparser.py
+++ output_parser/jsonoutputparser.py
@@ -1,12 +1,18 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from dotenv import load_dotenv
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 
 
 load_dotenv()
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-4-31B-it",
+    task="text-generation"
+)
 
-model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+model = ChatHuggingFace(llm=llm)
+# model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
 
 parser = JsonOutputParser()
 
